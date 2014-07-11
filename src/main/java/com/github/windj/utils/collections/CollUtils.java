7824cbc117b8e5fc49c02c6bf432551368b53c67
windj007 <windj007@gmail.com>
2014-07-11 06:53:08 +0400

    add some new utilities for list creation

--- src/main/java/com/github/windj/utils/collections/CollUtils.java
+++ src/main/java/com/github/windj/utils/collections/CollUtils.java
@@ -1,8 +1,10 @@
 package com.github.windj.utils.collections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class CollUtils {
@@ -12,5 +14,9 @@ public final class CollUtils {
 
 	public static <T> Set<T> constantSet(T... elems) {
 		return Collections.unmodifiableSet(new HashSet<T>(Arrays.asList(elems)));
+	}
+
+	public static <T> List<T> listFromValues(T... values) {
+		return new ArrayList<T>(Arrays.asList(values));
 	}
 }
